ebd10ac78dff95ce1ca44968cfd4505f9ea5ceb6
itstrH <trannguyenbahuy2908@gmail.com>
2025-05-26 21:41:48 +0700

    finished lesson 14.6: React onClick

--- 14.0 props-start/src/components/TabButton.jsx
+++ 14.0 props-start/src/components/TabButton.jsx
@@ -1,7 +1,22 @@
-export default function TabButton({ children }) {
+import { func } from "prop-types"
+
+export default function TabButton({ children }) { 
+
+    // js
+    // const btn = document.querySelector("button");
+    // btn.addEventListener("click", () => {
+    //     // lam viec
+    // })
+    
+    function handleClick() {
+        alert("Button duoc click!");
+    }
+
     return (
         <li>
-            <button>{children}</button>
+            <button onClick={handleClick}>{children}</button>
+            {/* 2nd way */}
+            {/* <button onClick={() => handleClick()}>{children}</button> */}
         </li>
     )
 }
