950411daaaf1ae6322938ec9fe4e3877b5adc4f0
itstrH <trannguyenbahuy2908@gmail.com>
2025-06-03 09:30:53 +0700

    finished lesson 14.15: completed react state exercise 3

--- 14.15.1baitap-react03/src/App.jsx
+++ 14.15.1baitap-react03/src/App.jsx
@@ -1,11 +1,19 @@
+import { use } from "react";
 import {useState} from "react";
 
 //bài tập 03 khoá học react js tại https://tuhoc.cc
 export default function App() {
+  const[isActive, setIsActive] = useState(false)
+
+  function handleToggle() {
+    // setIsActive((prev) => !prev) //truyen vao 1 tham so co ten bat ky
+    setIsActive((activeState) => !activeState)
+  }
+
   return (
     <div className="container">
-      <p>Click vào em!</p>
-      <button>Toggle btn</button>
+      <p className={isActive ? "active" : undefined}>Click vào em!</p>
+      <button onClick={handleToggle}>Toggle btn</button>
     </div>
   );
 }
