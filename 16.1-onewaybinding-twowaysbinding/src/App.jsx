03e04b16213598775bb3870a993dce5cf058a977
itstrH <trannguyenbahuy2908@gmail.com>
2025-06-06 17:23:16 +0700

    finished lesson 16.1: learn about 1 way binding and 2 ways binding

---
+++ 16.1-onewaybinding-twowaysbinding/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // khai bao state de luu gia tri input
+  const[playerName, setPlayerName] = useState("Player 1");
+  // ham xu ly khi nguoi dung nhap lieu
+  const handleChange = (e) => {
+    setPlayerName(e.target.value);
+  }
+  return (
+    <>  
+      <h2>Nhap ten Player</h2>
+      <input type="text" value={playerName} onChange={handleChange} />
+      <p>Ten ban da nhap: {playerName}</p>
+    </>
+  )
+}
+
+export default App
